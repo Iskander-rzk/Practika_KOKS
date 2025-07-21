4e60f8a171754f75ff596376a6294c4bfc73c168
Iskander-rzk <76554104+Iskander-rzk@users.noreply.github.com>
2025-07-21 23:54:58 +0700

    Add dependency injection (#5)
    * .
    
    * .

--- app/api/endpoints/service.py
+++ app/api/endpoints/service.py
@@ -1,52 +1,42 @@
 from fastapi import APIRouter, UploadFile, File, Request
 from fastapi.responses import RedirectResponse
-from app.core.templates import templates
 from app.controller import controller
-
+from app.controller.controller import get_current_user
 
 router = APIRouter()
 
+@router.post("/export/")
+async def export_ips(request: Request):
+    user = get_current_user(request)
+    if not user:
+        return RedirectResponse(url="/user/login/", status_code=303)
 
-@router.post("/export")
-def export_ips(request: Request):
     response = controller.export_ips()
-    if response.error:
-        return templates.TemplateResponse(
-            "index.html",
-            {
-                "request": request,
-                "error": response.error.string(),
-                "ip_addresses": response.ip_addresses
-            }
-        )
-    return RedirectResponse(url="/?message=IPs exported successfully", status_code=303)
+    return RedirectResponse(
+        url=f"/user/{user.id}/?message=IPs+exported" if not response.error else f"/user/{user.id}/?error={response.error.string()}",
+        status_code=303
+    )
 
+@router.post("/import/")
+async def import_ips(request: Request):
+    user = get_current_user(request)
+    if not user:
+        return RedirectResponse(url="/user/login/", status_code=303)
 
-@router.post("/import")
-def import_ips(request: Request):
     response = controller.import_ips()
-    if response.error:
-        return templates.TemplateResponse(
-            "index.html",
-            {
-                "request": request,
-                "error": response.error.string(),
-                "ip_addresses": response.ip_addresses
-            }
-        )
-    return RedirectResponse(url="/?message=IPs imported successfully", status_code=303)
+    return RedirectResponse(
+        url=f"/user/{user.id}/?message=IPs+imported" if not response.error else f"/user/{user.id}/?error={response.error.string()}",
+        status_code=303
+    )
 
+@router.post("/upload/")
+async def upload_file(request: Request, file: UploadFile = File(...)):
+    user = get_current_user(request)
+    if not user:
+        return RedirectResponse(url="/user/login/", status_code=303)
 
-@router.post("/upload")
-def upload_file(request: Request, file: UploadFile = File(...)):
     response = controller.upload_and_import(file)
-    if response.error:
-        return templates.TemplateResponse(
-            "index.html",
-            {
-                "request": request,
-                "error": response.error.string(),
-                "ip_addresses": response.ip_addresses
-            }
-        )
-    return RedirectResponse(url="/?message=IPs upload successfully", status_code=303)
+    return RedirectResponse(
+        url=f"/user/{user.id}/?message=File+uploaded" if not response.error else f"/user/{user.id}/?error={response.error.string()}",
+        status_code=303
+    )
